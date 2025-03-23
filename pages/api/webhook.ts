30a52a46005d31c494152643af9d785ff55d259f
Roxana Zwicky <roxana_zwicky@roxynomad.com>
2025-03-23 09:51:55 +0100

    fix: STRIPE_KEYS

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_TEST_KEY as string, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2025-02-24.acacia",
 });
 
